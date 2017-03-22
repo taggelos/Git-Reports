deaa0035e8423388877f812c33ffeb3c7f69fe12
Aggelos Toumasis <aggelos5991@gmail.com>
2017-03-22 15:35:24 +0200

    repository name sto html :imp: :baby: :zzz:

--- src/MainReport.java
+++ src/MainReport.java
@@ -24,7 +24,7 @@ public class MainReport {
             bw.write("<title> Exercise-1 </title>");
             bw.write("<style>table, th, td {border: 1px solid black; border-collapse: collapse; white-space: nowrap;}th, td { padding: 5px; text-align: left; }</style>");
             bw.write("</head> <body bgcolor=\"#fcf5ef\" >");
-            bw.write("<h2><font color = \"red\"> Report for </font> <a target=\"_blank\" href=\""+ args[0] +"\"> Git repository </a></h2>");
+            bw.write("<h2><font color = \"red\"> Report for  <a target=\"_blank\" href=\""+ args[0] +"\"> "+args[0].substring(args[0].lastIndexOf("\\")+1)+"</a> repository </font></h2>");
             bw.write("<table bgcolor=\"#FFFFFF\" style=\"width:15%\">");
             
             MainReport obj = new MainReport();
